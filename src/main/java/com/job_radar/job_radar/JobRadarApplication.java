c600d300546d21d388579536082ea9cb6a4cec21
Hyeyeon-Kim <clscls2530@naver.com>
2025-12-03 19:29:04 +0900

    init

---
+++ src/main/java/com/job_radar/job_radar/JobRadarApplication.java
@@ -0,0 +1,13 @@
+package com.job_radar.job_radar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobRadarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobRadarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/job_radar/job_radar/JobRadarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.job_radar.job_radar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobRadarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
